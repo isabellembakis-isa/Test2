8a6d6ee453de3764fe56794b887884650b13ebb4
isabelle <isabellembakis@gmail.com>
2026-05-10 17:04:58 +0200

    Add initial HTML structure and implement divide function in index.js

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function addd(a, b){
 function multiply(a, b){
     return a*b;
 }
+
+function divide(a, b){
+    if(b === 0){
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}
